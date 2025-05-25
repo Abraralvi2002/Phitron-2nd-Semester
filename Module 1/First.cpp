2127496d32512005b4032d32afae958b4b2fd101
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-25 16:47:10 +0600

    A new Journey for C++ has started

---
+++ Module 1/First.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    std :: cout << "My First C++"<< std::endl;
+    int a = 10;
+    double b = 10.45;
+    char c = 'D';
+    std:: cout << a << " " << b << " " << c << std:: endl;
+}
