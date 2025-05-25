772fb225846800af06b2069b20a2219524f5da14
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-25 18:13:48 +0600

    learned about min and max function

---
+++ Module 1/Min_max_swap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int a = 10,b = 3,c=23,d=12,e=32;
+
+    cout << min({a,b,c,d,e}) << endl;
+    cout << max({a,b,c,d,e}) << endl;
+    cout << min(a,b) << endl;
+    cout << max(a,b) << endl;
+
+    return 0;
+}
