7b0def4d10cd7b96e4ad6fc0cccfd3a5f895a150
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-25 17:08:00 +0600

    Learned about typecasting

--- Module 1/Input.cpp
+++ Module 1/Input.cpp
@@ -4,6 +4,11 @@ int main(){
     int a,b;
     cin >> a >> b;
     cout << a + b << endl;
-
+    char c;
+    cin >> c;
+    cout << c << endl;
+    cout << (int)c << endl;
+    int d = c;
+    cout << (char)d << endl;
     return 0;
 }
