ea05baaa8bbd10c76af8b8e97a14218ff2194bc2
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-28 14:38:25 +0600

    3rd problem solved

---
+++ Practice Day 1/F_Reversing.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i = 0;i<n;i++){
+        cin>>ar[i];
+    }
+    for(int i = 0;i<n/2;i++){
+        swap(ar[i],ar[n-i-1]);
+    }
+    for(int i = 0;i<n;i++){
+        cout<<ar[i]<<" ";
+    }
+    return 0;
+}
