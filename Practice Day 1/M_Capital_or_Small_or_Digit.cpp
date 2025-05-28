d869423fb43ee663ad2ce13981178c3ea81254e7
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-28 14:19:44 +0600

    First one solved of sheet 1 from codeforces

---
+++ Practice Day 1/M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char a;
+    cin>>a;
+    if(a>= '0' && a<= '9'){
+        cout<<"IS DIGIT";
+    }else{
+        cout<<"ALPHA"<<endl;
+        if(a>='A' && a<='Z'){
+            cout<<"IS CAPITAL";
+        }else if(a>='a' && a<='z'){
+            cout<<"IS SMALL";
+        }
+    }
+    return 0;
+}
