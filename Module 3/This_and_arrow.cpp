e6ae54cf1dcdcab8295ed374e528f9e3381a15cc
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-28 18:54:13 +0600

    learned about this and arrow

---
+++ Module 3/This_and_arrow.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+    string name;
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(string name,int roll,int cls,double gpa){
+        this->name = name;
+        (*this).roll = roll;
+        (*this).cls = cls;
+        this->gpa = gpa;
+    }
+
+};
+int main(){
+    Student a("Joshim",5,7,4.50);
+    Student b("Hashem",3,7,4.98);
+
+    cout<<a.name<<" "<<a.roll<<" "<<a.cls<<" "<<a.gpa<<endl;
+    cout<<b.name<<" "<<b.roll<<" "<<b.cls<<" "<<b.gpa<<endl;
+    return 0;
+}
