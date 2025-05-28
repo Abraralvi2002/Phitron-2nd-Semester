24e7caa877f1bd90cf322e6bfae85996036f5f18
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-05-28 11:47:00 +0600

    Dynamic variable

---
+++ Module 2/Dynamic_variable.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int *p = new int;
+    int *q = new int;
+    cout<<"Enter Two Integer"<<endl;
+    cin>>*p>>*q;
+    cout<<*p+*q<<endl;
+    return 0;
+}
