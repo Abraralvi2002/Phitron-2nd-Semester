6d7628466fc192b91112b3a946f3b711dd54707d
Abraralvi2002 <abrarsiddiqui734@gmail.com>
2025-08-01 13:57:21 +0600

    solved another one

---
+++ Assignment 4/Perfect_Binary_Tree.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* input_tree(){
+    int val;
+    cin>>val;
+
+    Node* root;
+
+    if(val == -1){
+        root = NULL;
+    }else{
+        root = new Node(val);
+    }
+    queue<Node*> q;
+    if(root){
+        q.push(root);
+    }
+
+    while(!q.empty()){
+
+        Node* f = q.front();
+        q.pop();
+
+        int l,r;
+        cin>>l>>r;
+        Node* myleft,* myright;
+
+        if(l == -1){
+            myleft = NULL;
+        }else{
+            myleft = new Node(l);
+        }
+
+        if(r == -1){
+            myright = NULL;
+        }else{ 
+            myright = new Node(r);
+        }
+
+        f->left = myleft;
+        f->right = myright;
+
+        if(f->left){
+            q.push(f->left);
+        }
+        if(f->right){
+            q.push(f->right);
+        }
+    }
+
+    return root;
+}
+
+int maximum_depth(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int l = maximum_depth(root->left);
+    int r = maximum_depth(root->right);
+
+    return max(l,r) + 1;
+}
+
+int counting_nodes(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int l = counting_nodes(root->left);
+    int r = counting_nodes(root->right);
+
+    return l+r+1;
+}
+
+int main(){
+    
+    Node* root = input_tree();
+    int nodes = counting_nodes(root);
+    int depth = maximum_depth(root);
+
+    int formula = pow(2,depth) - 1;
+
+    if(formula == nodes){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+
+    return 0;
+}
